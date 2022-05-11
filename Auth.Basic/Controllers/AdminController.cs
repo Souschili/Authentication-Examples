081cc7b647e35b33f63903f84c9106dfb3ca75a9
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-12 01:53:38 +0200

    Add project files.

---
+++ Auth.Basic/Controllers/AdminController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Auth.Basic.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            //при переходе, получаем параметр из строки адресса и потом используем его в пост запросе
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                //возращаем модель на страницк запроса
+                return View(viewModel);
+            }
+
+            //список клаймов
+            var claims = new List<Claim>
+            {
+                new Claim("Demo","Value")
+            };
+
+            //обязательный параметр для аутентификации клайм айдентити
+            var claimIdentity=new ClaimsIdentity(claims,"Cookie");
+
+            //аутентификация через клаймы
+            ClaimsPrincipal claimPrincipal=new ClaimsPrincipal(claimIdentity);
+            await HttpContext.SignInAsync("Cookie", claimPrincipal);
+
+            // если все хорошо то редиректим 
+            return Redirect(viewModel.ReturnUrl);
+        }
+
+
+
+        public async Task<IActionResult> LogOffAsync()
+        {
+            //разлогин пользователя и возврат на домашнюю страницу
+            await HttpContext.SignOutAsync("Cookie");
+
+
+            return Redirect("/Home/Index");
+        }
+    }
+}
---
+++ Auth.Basic/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Auth.Basic
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Cookie")
+                .AddCookie("Cookie", conf =>
+                {
+                    conf.LoginPath = "/Admin/Login";
+                });
+
+
+            services.AddAuthorization();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
